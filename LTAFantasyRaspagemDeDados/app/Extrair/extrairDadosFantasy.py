fc1e6db1edcf83ad45b175de77122745da5b4a9f
Artur trombim <arturtrombim.dev@gmail.com>
2025-05-03 18:29:08 -0300

    fix: ajuste no extrator de jogador  para passar user_agent

--- LTAFantasyRaspagemDeDados/app/Extrair/extrairDadosFantasy.py
+++ LTAFantasyRaspagemDeDados/app/Extrair/extrairDadosFantasy.py
@@ -24,12 +24,18 @@ def extrair_dados_fantasy(cookieid):
             "valor_atual": "div.flex.text-sm.items-center.font-kurdis.text-right.justify-end span.font-kurdis-extra-bold",
             "flutuacao": "div.flex.text-sm.items-center.font-kurdis.text-right.justify-end span.font-red-hat-display-semi-bold"
         }
+
         jogadores =[]
         tentativas = 3
         for tentativa in range(tentativas):
             with sync_playwright() as p:
-                browser = p.chromium.launch(headless=False)
-                context = browser.new_context()
+                browser = p.chromium.launch(headless=True)
+                context = browser.new_context(
+                    user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                    viewport={"width": 1280, "height": 800},
+                    java_script_enabled=True,
+                    locale="pt-BR"
+                )
                 context.add_cookies(cookies)
                 page = context.new_page()
                 try:
